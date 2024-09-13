ae1012dec6eb683b567affc48ec9d7507d2df0dd
brandy-wang <166099828+brandy-wang@users.noreply.github.com>
2024-09-12 17:02:48 -0700

    RaisePower

---
+++ lab01/RaisePower.java
@@ -0,0 +1,11 @@
+public class RaisePower {
+  public static void main(String[] args) {
+    double base = 3.0;
+    double exponent = 5.0;
+
+    double result = Math.pow(base, exponent);
+
+    System.out.println(base + " raised to the power of " + exponent + " is " + result);
+  }
+}
+
