8efba0a2164f6ec0ad3ca3612282a58046f17598
brandy-wang <166099828+brandy-wang@users.noreply.github.com>
2024-10-02 11:56:07 -0700

    lab number 3

--- lab03/newshoe/test/NewShoeTest.java
+++ lab03/newshoe/test/NewShoeTest.java
@@ -1,70 +1,69 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class NewShoeTest {
+  // Declare test objects
+  private NewShoe shoe1;
+  private NewShoe shoe2;
 
 
-  @Test
-  public void test_constructor() {
-    //NewShoe test_shoe = new NewShoe("", Color.GREEN, "Jimmy Choo", 7);
+  @Before
+  public void setUp() throws Exception {
+    // Initialize test objects before each test
+    shoe1 = new NewShoe("heels", Color.GREEN, "Jimmy Choo", 7.5);
+    // Example of shoe
+    shoe2 = new NewShoe("flats", Color.RED, "Nike", 10.5);
+
   }
-  @Test
-  public void test_constructor2() {
-
-  }
-
-
 
   @Test
   public void getType() {
-    NewShoe test_shoe = new NewShoe("heels", Color.GREEN, "Jimmy Choo", 7);
-    assertEquals("Testing getType getter method", "heels", test_shoe.getType());
-  }
-  @Test
-  public void getType2() {
-    NewShoe test_shoe = new NewShoe("flats", Color.GREEN, "Jimmy Choo", 7);
-    assertEquals("Testing getType getter method again", "flats", test_shoe.getType());
+    //test1:check whether return value-type is correct
+    assertEquals("heels", shoe1.getType());
+    //test2:check for not empty
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+      //pass empty string to trigger exception
+      new NewShoe("", Color.GREEN, "Jimmy Choo", 7.5);
+    });
+    assertEquals("type cannot be empty", exception.getMessage());
   }
 
   @Test
   public void getColor() {
-    NewShoe test_shoe = new NewShoe("heels", Color.RED, "Dior", 7);
-    assertEquals("Testing getColor getter method", Color.RED, test_shoe.getColor());
+    //test1:check whether return value-type is correct for shoe1
+    assertEquals(Color.GREEN,shoe1.getColor());
+    //test2:check whether return value-type is correct for shoe2
+    assertEquals(Color.RED,shoe2.getColor());
   }
-  @Test
-  public void getColor2() {
-    NewShoe test_shoe = new NewShoe("heels", Color.BLUE, "Dior", 8);
-    assertEquals("Testing getColor getter method again", Color.BLUE, test_shoe.getColor());
-  }
+
   @Test
   public void getBrand() {
-    NewShoe test_shoe = new NewShoe("flats", Color.RED, "Nike", 10);
-    assertEquals("Testing getBrand getter method", "Nike", test_shoe.getBrand());
+    //test1:check whether return value-brand is correct for shoe1
+    assertEquals("Jimmy Choo",shoe1.getBrand());
+    //test2:check whether return value-brand is not correct for shoe2
+    assertEquals("Nike",shoe2.getBrand());
   }
-  @Test
-  public void getBrand2() {
-    NewShoe test_shoe = new NewShoe("flats", Color.RED, "Levi's", 10);
-    assertEquals("Testing getBrand getter method again", "Levi's", test_shoe.getBrand());
-  }
+
   @Test
   public void getSize() {
-    NewShoe test_shoe = new NewShoe("Sneaker", Color.BLUE, "Vans", 10);
-    assertEquals("Testing getSize getter method", 10, test_shoe.getSize(), 0);
+    //test1:check whether return value-size is correct for shoe1
+    assertEquals(7.5,shoe1.getSize(),0);
+    //test2:check whether return value-size is correct for shoe2
+    assertEquals(10.5,shoe2.getSize(),0);
+
   }
-  @Test
-  public void getSize2() {
-    NewShoe test_shoe = new NewShoe("Sandal", Color.BLUE, "Birkenstock", 9);
-    assertEquals("Testing getSize getter method again", 9, test_shoe.getSize(), 0);
-  }
+
   @Test
   public void testToString() {
-    NewShoe test_shoe = new NewShoe("Sandal", Color.ORANGE, "Birkenstock", 9);
-    assertEquals("Testing toString method", "Type: Sandal" +"\n" +"Color: To Be Determined" + "\n" + "Brand: Birkenstock" + "\n" + "Size: 9.00", test_shoe.toString());
-  }
-  @Test
-  public void testToString2() {
-    NewShoe test_shoe = new NewShoe("Sandal", Color.BLUE, "Birkenstock", 9);
-    assertEquals("Testing toString method again", "Type: Sandal" +"\n" +"Color: blUE" + "\n" + "Brand: Birkenstock" + "\n" + "Size: 9.00", test_shoe.toString());
+    //test1:expected version for shoe1
+    String expected = "Type: heels" +"\n" + "Color: GReen" +"\n" + "Brand: Jimmy Choo" +"\n" + "Size: 7.50";
+    assertEquals(expected,shoe1.toString());
+    //test2:unexpected version for shoe2
+    String unexpected = "Type: flats" +"\n" + "Color: rEd" +"\n" + "Brand: Nike" +"\n" + "Size: 10.50";
+    assertEquals(unexpected,shoe2.toString());
+
+
   }
 }
